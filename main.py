12691ff62f28522650f5c5c472be283606049cdb
shreyansh-web <shreyansh487@gmail.com>
2025-08-10 19:26:46 +0530

    path params enum

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 #Define model Item 
 class Item(BaseModel):
     name: str
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 #Instatiate app
 app = FastAPI();
@@ -13,10 +19,25 @@ app = FastAPI();
 def root():
     return {"message:" : "Hello World"}
 
+@app.get("/items/{item}")
+async def items(item: str):
+    return {"item" : item}
+
 #Takes in query parameter name
 @app.get("/hello")
 def hello(name: str = "Shreyansh"):
     return {"message" : f"Hello {name}"}
+
+@app.get("/models/{model_name}")
+def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet: 
+        return {"model name": model_name, "message": "Deep learing ftw"}
+    if model_name.value == "lenet": 
+        return {"model name": model_name, "message": "lenet is good"}
+
+    return {"model name": model_name, "message": "Machine learning is great"}
+
+
 
 
 @app.post("/")
